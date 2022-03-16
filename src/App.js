0e29ab4ad0fde65b1d76f2dfe0b523fcbdf536d3
ullash4 <programmerullash@gmail.com>
2022-03-16 19:50:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const prodct=[
+    {name:'Laptop', price:'30000'},
+    {name:'Iphone', price:'400000'},
+    {name:'Watch', price:'300'},
+    {name:'Sunglass', price:'200'},
+    {name:'glass', price:'200'},
+    {name:'glass', price:'200'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App"> 
+      {
+        prodct.map(pro=> <Products nam={pro.name} price={pro.price}></Products>)
+      }
     </div>
   );
+};
+function Products({nam, price}) {
+  return (
+    <div className='product'>
+      <h2>Name: {nam}</h2>
+      <p>Price: {price}</p>
+    </div>
+  )
 }
 
 export default App;
+
+
+// const nayok = ['William Rock', 'Vladimir Putin', 'Obaidul Kadir', 'Salman', 'Jhanker Mahbub'];
+ {/* <Ullash name={nayok[0]} para="I am full time ReactJs developer. Wish me luck!!"></Ullash>
+      <Ullash name={nayok[1]} para="I am full time world attaker. Wish me luck!!"></Ullash>
+      <Ullash name={nayok[2]} para="I am full time President. Wish me luck!!"></Ullash> */}
+     {/* {
+        nayok.map(nayok=><Ullash name={nayok} para="I am full time ReactJs developer. Wish me luck!!"></Ullash>)
+      } */}
